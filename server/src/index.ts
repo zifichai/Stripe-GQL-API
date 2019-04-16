896444ae9de5b3df0e133f064b6b923d0213954d
dev <mabbott19994@gmail.com>
2019-04-17 00:47:28 +0100

    added context session

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import { ApolloServer } from 'apollo-server-express';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as session from 'express-session';
+import { resolvers } from './schema/resolves';
+import { typeDefs } from './schema/typeDefs';
+
+
+const startServer = async () => {
+  let con = await mongoose.connect('mongodb://localhost:27017/stripe-example', { useNewUrlParser: true });
+  const apolloSever = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req })
+  });
+
+
+  const app: express.Application = express();
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: "sdSDAFDAF"
+    })
+  );
+
+  apolloSever.applyMiddleware({ app });
+
+  const port = process.env.PORT || 4000;
+  if (con) app.listen(port, () =>
+    console.log(`server ready at http://localhost:${port}${apolloSever.graphqlPath}`));
+
+}
+
+startServer();
