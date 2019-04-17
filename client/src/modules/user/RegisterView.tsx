cf208588857ef9ba793c38c565f3fae7725cead2
dev <mabbott19994@gmail.com>
2019-04-17 02:25:56 +0100

    added register mutation

--- client/src/modules/user/RegisterView.tsx
+++ client/src/modules/user/RegisterView.tsx
@@ -1,6 +1,16 @@
 import React, { PureComponent } from 'react'
+import { Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { RegisterMutation, RegisterMutationVariables } from '../../schemaTypes';
+import { RouteComponentProps } from 'react-router';
 
-export default class RegisterView extends PureComponent {
+const registerMutation = gql`
+  mutation RegisterMutation($email: String!, $password:String!) {
+    register(email: $email, password: $password)
+  }
+`;
+
+export default class RegisterView extends PureComponent<RouteComponentProps<{}>> {
   state = {
     email: '',
     password: ''
@@ -14,16 +24,34 @@ export default class RegisterView extends PureComponent {
     });
   }
 
+  onSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('submitted')
+  }
+
   render() {
     const { email, password } = this.state;
     return (
-      <form>
-        <input type="text" placeholder="email" name="email" value={email} onChange={this.handleChange} />
-        <input type="password" placeholder="password" name="password" value={password} onChange={this.handleChange} />
-        <button onClick={() => console.log('submitted')}>Login</button>
-      </form>
-    )
+      <Mutation<RegisterMutation, RegisterMutationVariables> mutation={registerMutation}>
+        {mutate => (
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <div>
+              <input type="text" placeholder="email" name="email" value={email} onChange={this.handleChange} />
+            </div>
+            <div>
+              <input type="password" placeholder="password" name="password" value={password} onChange={this.handleChange} />
+            </div>
+            <div>
+              <button onClick={async () => {
+                const response = await mutate({ variables: { email, password } })
+                console.log(response);
+                this.props.history.push('/login');
+              }}>Login</button>
+            </div>
+          </div>
+        )}
+      </Mutation>
+    );
   }
 }
 
-}
