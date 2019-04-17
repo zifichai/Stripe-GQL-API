fd38fa0a75933336bb9962269b4d2f9b41173fdc
dev <mabbott19994@gmail.com>
2019-04-17 01:10:08 +0100

    Created register view

---
+++ client/src/modules/user/RegisterView.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class RegisterView extends PureComponent {
+  render() {
+    return (
+      <div>
+        Register view
+      </div>
+    )
+  }
+}
