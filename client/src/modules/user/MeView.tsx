4f27ab55949d0f9cf6b7af04c828ac12fbc2c3b0
dev <mabbott19994@gmail.com>
2019-04-17 02:26:17 +0100

    Added me mutation

--- client/src/modules/user/MeView.tsx
+++ client/src/modules/user/MeView.tsx
@@ -1,11 +1,37 @@
 import React, { PureComponent } from 'react'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { MeQuery } from '../../schemaTypes';
+
+const meQuery = gql`
+  query MeQuery{
+    me {
+      _id
+      email
+    }
+  }
+`;
 
 export default class MeView extends PureComponent {
   render() {
     return (
-      <div>
-        Me View
-      </div>
+      <Query<MeQuery> query={meQuery}>
+        {({ data, loading }) => {
+          if (loading) {
+            return null;
+          }
+
+          if (!data) {
+            return <div>data is undefined</div>;
+          }
+
+          if (!data.me) {
+            return <div>received no user</div>;
+          }
+
+          return <div>{data.me.email}</div>;
+        }}
+      </Query>
     )
   }
 }
