19154c7cccf0cdf268ba5c960b6cdc89f56baecc
dev <mabbott19994@gmail.com>
2019-04-17 02:28:41 +0100

    generated types from schema.json dump

---
+++ client/src/schemaTypes.ts
@@ -0,0 +1,70 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: LoginMutation
+// ====================================================
+
+export interface LoginMutation_login {
+  __typename: "User";
+  _id: string;
+  email: string;
+  createdAt: string;
+}
+
+export interface LoginMutation {
+  login: LoginMutation_login | null;
+}
+
+export interface LoginMutationVariables {
+  email: string;
+  password: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MeQuery
+// ====================================================
+
+export interface MeQuery_me {
+  __typename: "User";
+  _id: string;
+  email: string;
+}
+
+export interface MeQuery {
+  me: MeQuery_me | null;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: RegisterMutation
+// ====================================================
+
+export interface RegisterMutation {
+  register: boolean;
+}
+
+export interface RegisterMutationVariables {
+  email: string;
+  password: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
