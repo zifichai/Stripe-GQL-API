9151d6b20a8321a03a3b22abc8c2e213ba629577
dev <mabbott19994@gmail.com>
2019-04-17 01:10:33 +0100

    Added me view

---
+++ client/src/modules/user/MeView.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class MeView extends PureComponent {
+  render() {
+    return (
+      <div>
+        Me View
+      </div>
+    )
+  }
+}
