b668937cf974315e1c5ecc5af34663bac4d1ceca
dev <mabbott19994@gmail.com>
2019-04-17 01:12:56 +0100

    added form to register view

--- client/src/modules/user/RegisterView.tsx
+++ client/src/modules/user/RegisterView.tsx
@@ -1,11 +1,29 @@
 import React, { PureComponent } from 'react'
 
 export default class RegisterView extends PureComponent {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = (e: any) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
-      <div>
-        Register view
-      </div>
+      <form>
+        <input type="text" placeholder="email" name="email" value={email} onChange={this.handleChange} />
+        <input type="password" placeholder="password" name="password" value={password} onChange={this.handleChange} />
+        <button onClick={() => console.log('submitted')}>Login</button>
+      </form>
     )
   }
+}
+
 }
