6b3a1e275c697449ecc80525e4e7666279c0a72f
dev <mabbott19994@gmail.com>
2019-04-21 21:57:14 +0100

    added stripe details

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -3,11 +3,15 @@ import * as mongoose from 'mongoose';
 export interface IUser extends mongoose.Document {
   email: string;
   password: string;
+  stripeId: string;
+  typeOfUser: string;
 }
 
 export const UserSchema: mongoose.Schema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  stripeId: { type: String },
+  typeOfUser: { type: String, default: 'free-trial' }
 
 }, { timestamps: true });
 
