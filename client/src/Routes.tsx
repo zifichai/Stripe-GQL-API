d53aa24293af1f685bc37a27d12a29de6ac585ef
dev <mabbott19994@gmail.com>
2019-04-21 21:56:15 +0100

    added stripe route

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginView from './modules/user/LoginView';
 import RegisterView from './modules/user/RegisterView';
 import MeView from './modules/user/MeView';
+import SubscribeUser from './modules/account/SubscribeUser';
 
 export default class Routes extends PureComponent {
   render() {
@@ -12,6 +13,8 @@ export default class Routes extends PureComponent {
           <Route path="/login" component={LoginView} />
           <Route path="/register" component={RegisterView} />
           <Route path="/me" component={MeView} />
+          <Route path="/subscription" component={SubscribeUser} />
+
         </Switch>
       </BrowserRouter>
     )
