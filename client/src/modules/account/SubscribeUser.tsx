953740c9e6108706f3d2c699f706ec58cd5125f1
dev <mabbott19994@gmail.com>
2019-04-22 14:29:44 +0100

    Changed layout to contain user fragment

--- client/src/modules/account/SubscribeUser.tsx
+++ client/src/modules/account/SubscribeUser.tsx
@@ -3,16 +3,16 @@ import StripeCheckout from 'react-stripe-checkout';
 import { Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import { CreateSubscriptionMutationVariables, CreateSubscriptionMutation } from '../../schemaTypes';
+import { userFragment } from '../../graphql/fragements/UserFragment';
 
 
 const createSubscriptionMutation = gql`
-  mutation CreateSubscriptionMutation($source: String!) {
-    createSubscripton(source: $source) {
-      _id, 
-      email
-      typeOfUser
+  mutation CreateSubscriptionMutation($source: String!, $ccLast4: String!) {
+    createSubscripton(source: $source, ccLast4: $ccLast4) {
+      ...UserInfo
     }
   }
+  ${userFragment}
 `;
 
 
@@ -24,7 +24,7 @@ export default class SubscrbeUser extends React.PureComponent {
         {mutate => (
           <StripeCheckout
             token={async (token) => {
-              const response = await mutate({ variables: { source: token.id } });
+              const response = await mutate({ variables: { source: token.id, ccLast4: token.card.last4 } });
               console.log(response);
             }}
             stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE!}
