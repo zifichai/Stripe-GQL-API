2a0087a147d5289e44b8b1683006327230ac050e
dev <mabbott19994@gmail.com>
2019-04-22 14:32:54 +0100

    misc

--- client/src/graphql/queries/me.tsx
+++ client/src/graphql/queries/me.tsx
@@ -1,11 +1,12 @@
 import { gql } from "apollo-boost";
+import { userFragment } from "../fragements/UserFragment";
 
 export const meQuery = gql`
   query MeQuery{
     me {
-      _id
-      email
-      typeOfUser
+      ...UserInfo
     }
   }
+
+  ${userFragment}
 `;
